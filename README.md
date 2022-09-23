0c9c020e5e4f46583c107f8cd90067a54e789a04
NChanard <nchanard@univ-tlse3.fr>
2022-09-23 14:35:27 +0200

    Add tests

--- README.md
+++ README.md
@@ -6,6 +6,8 @@
 <!-- badges: start -->
 
 [![R-CMD-check](https://github.com/NChanard/ColorTK/actions/workflows/R-CMD-check.yaml/badge.svg)](https://github.com/NChanard/ColorTK/actions/workflows/R-CMD-check.yaml)
+[![Codecov test
+coverage](https://codecov.io/gh/NChanard/ColorTK/branch/master/graph/badge.svg)](https://app.codecov.io/gh/NChanard/ColorTK?branch=master)
 <!-- badges: end -->
 
 Set of colour manipulation tools.
---
+++ tests/testthat/tests.R
@@ -0,0 +1,180 @@
+Brighten("#ebb0107f",alpha.bln=TRUE)
+
+Col2Hex(c(43.8,0.873,0.492,0.498),alpha.bln=TRUE)
+
+Col2Hsl("#ebb0107f",alpha.bln=TRUE)
+
+Col2Rgb(c("green","red"),alpha.bln=TRUE)
+
+Colorize("#ebb0107f",alpha.bln=TRUE)
+
+ConvertColorFormat("#ebb0107f", alpha.bln=TRUE, format.str="Rgb")
+ConvertColorFormat("#ebb0107f", alpha.bln=TRUE, format.str="Hsl")
+ConvertColorFormat(c(235,176,16,127), alpha.bln=TRUE, format.str="Hex")
+ConvertColorFormat(c(235,176,16,127), alpha.bln=TRUE, format.str="Hsl")
+ConvertColorFormat(c(43.8,0.873,0.492,0.498), alpha.bln=TRUE, format.str="Rgb")
+ConvertColorFormat(c(43.8,0.873,0.492,0.498), alpha.bln=TRUE, format.str="Hex")
+
+Darken("#ebb0107f",alpha.bln=TRUE)
+
+GetColorFormat("#ebb0107f")
+GetColorFormat(c(235,176,16,127))
+GetColorFormat(c(43.8,0.873,0.492,0.498))
+GetColorFormat("green")
+
+pal <- Hue(paletteLength.num=5)
+ggPalette(pal)
+
+Hex2Hsl("#ebb0107f",alpha.bln=TRUE)
+
+Hex2Rgb("#ebb0107f",alpha.bln=TRUE)
+
+Hsl2Hex(c(43.8,0.873,0.492,0.498),alpha.bln=TRUE)
+
+IsCol("red")
+IsCol("#FFFFFF")
+IsCol(c(125,125,125))
+IsCol(c(43.8,0.873,0.492))
+
+IsHex("red")
+IsHex("#FFFFFF")
+IsHex(c(125,125,125))
+IsHex(c(43.8,0.873,0.492))
+
+IsHsl("red")
+IsHsl("#FFFFFF")
+IsHsl(c(125,125,125))
+IsHsl(c(43.8,0.873,0.492))
+
+IsRgb("red")
+IsRgb("#FFFFFF")
+IsRgb(c(125,125,125))
+IsRgb(c(43.8,0.873,0.492))
+
+MutateColorParameter(
+    color.col="#ebb0107f",
+    operation.str="+",
+    parameter.str="light",
+    alpha.bln=TRUE,
+    prctMutation.num=0.2
+)
+
+MutateColorParameter(
+    color.col="#ebb0107f",
+    operation.str="-",
+    parameter.str="light",
+    alpha.bln=TRUE,
+    prctMutation.num=0.2
+)
+
+MutateColorParameter(
+    color.col="#ebb0107f",
+    operation.str="*",
+    parameter.str="light",
+    alpha.bln=TRUE,
+    prctMutation.num=0.2
+)
+
+MutateColorParameter(
+    color.col="#ebb0107f",
+    operation.str="/",
+    parameter.str="light",
+    alpha.bln=TRUE,
+    prctMutation.num=0.2
+)
+
+MutateColorParameter(
+    color.col="#ebb0107f",
+    operation.str="%*%",
+    parameter.str="light",
+    alpha.bln=TRUE,
+    prctMutation.num=0.2
+)
+
+
+MutateColorParameter(
+    color.col="#ebb0107f",
+    operation.str="%/%",
+    parameter.str="light",
+    alpha.bln=TRUE,
+    prctMutation.num=0.2
+)
+
+
+YlGnBu(9)
+
+YlOrBr(9)
+
+YlOrRd(9)
+
+PuBuGn(9)
+
+YlGn(9)
+
+OrRd(9)
+
+BuGn(9)
+
+GnBu(9)
+
+BuPu(9)
+
+PuBu(9)
+
+RdPu(9)
+
+PuRd(9)
+
+Greys(9)
+
+Oranges(9)
+
+Reds(9)
+
+Purples(9)
+
+Blues(9)
+
+Greens(9)
+
+RdBu(9)
+
+RdBu(9)
+
+RdYlBu(9)
+
+RdYlGn(9)
+
+PuOr(9)
+
+PiYg(9)
+
+PrGn(9)
+
+BrBG(9)
+
+Spectral(9)
+
+magma(9)
+
+inferno(9)
+
+plasma(9)
+
+viridis(9)
+
+cividis(9)
+
+rocket(9)
+
+mako(9)
+
+turbo(9)
+
+Pastelize("#ebb0107f",alpha.bln=TRUE)
+
+Rgb2Hex(c(235,176,16,127),alpha.bln=TRUE)
+
+Rgb2Hsl(c(235,176,16,127),alpha.bln=TRUE)
+
+Tarnished("#ebb0107f",alpha.bln=TRUE)
